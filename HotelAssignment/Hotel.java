9a7d0f677e4d9c67ce5b57ab8faa20e0220fbbba
dblelu <shmohanch@gmail.com>
2024-05-24 20:52:15 +0000

    c

--- HotelAssignment/Hotel.java
+++ HotelAssignment/Hotel.java
@@ -33,12 +33,71 @@ public class Hotel {
                 input = getInt(); 
             }
             employee(number);
+            br.close();
         } catch (IOException e) {
             System.out.println("Employee number not valid");
         }
     }
     public static void employee(int number) {
+        //Enter filler code that displays the name
+        boolean goBack = false;
+        while (!goBack) {
+            System.out.println("Enter instructions (1, 2, 3, 4)");
+            int input = getInt();
+            switch (input) {
+                case 1:
+                    makeReservations();
+                    break;
+                case 2:
+                    cancelReservation();
+                    break;
+                case 3:
+                    changeReservation();
+                    break;
+                case 4: 
+                    changePin();
+                    break;
+                default: 
+                    goBack = true;
+                    break;
+            }
+        }
+    }
+    public static makeReservations(int employee) {
+        int numberOfReservations, currentReservationDays[], currentReservationRooms[];
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("reservations.txt"));
+            numberOfReservations = Integer.parseInt(br.readLine());
+            currentReservationDays = new int[numberOfReservations];
+            currentReservationRooms = new int[numberOfReservations];
+            for (int i = 0; i < numberOfReservations; i++) {
+                currentReservationDays[i] = Integer.parseInt(br.readLine());
+                currentReservationRooms[i] = Integer.parseInt(br.readLine());
+            }
+        } catch (IOException e) {
+            System.out.println("Error " + e);
+            return false;
+        }
+        boolean valid = false;
+        while (!valid) {
+            System.out.println("Enter a day to reserve (int)");
+            int day = getInt();
+            if (!same(currentReservationDays, day)) valid = true;
+        }
+        valid = false;
+        while (!valid) {
+            System.out.println("Enter a room to reserve (int)");
+            int room = getInt();
+            if (!same(currentReservationRooms, room)) valid = true;
+        }
         
+    }
+    public static boolean same(int[] a, int x) {
+        for (int i = 0; i < a.length; i++) if (a[i] == x) return false;
+        return true;
+    }
+    public static canacelReservations() {
+
     }
     public static int getInt() {
         Scanner sc = new Scanner(System.in);
