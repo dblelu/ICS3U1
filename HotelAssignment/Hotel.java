dcd0992aa0511c970bc8960b9c410eeeb7f6cf49
dblelu <shmohanch@gmail.com>
2024-05-25 02:56:59 +0000

    c

--- HotelAssignment/Hotel.java
+++ HotelAssignment/Hotel.java
@@ -63,7 +63,7 @@ public class Hotel {
             }
         }
     }
-    public static makeReservations(int employee) {
+    public static boolean makeReservations(int employee) {
         int numberOfReservations, day, room, currentReservationDays[], currentReservationRooms[];
         try {
             BufferedReader br = new BufferedReader(new FileReader("reservations.txt"));
@@ -98,6 +98,7 @@ public class Hotel {
                 br.write(currentReservationDays[i]); br.newLine();
                 br.write(currentReservationRooms[i]); br.newLine();
             }
+            br.close();
         } catch (IOException e) {
             return false;
         }
@@ -107,8 +108,47 @@ public class Hotel {
         for (int i = 0; i < a.length; i++) if (a[i] == x) return false;
         return true;
     }
-    public static canacelReservations() {
-
+    public static boolean cancelReservations() {
+        int numberOfReservations, day, room, currentReservationDays[], currentReservationRooms[];
+        System.out.println("Enter a day to cancel (int)");
+        day = getInt();
+        System.out.println("Enter a room to cancel (int)");
+        room = getInt();
+        boolean valid = false;
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("reservations.txt"));
+            numberOfReservations = Integer.parseInt(br.readLine());
+            currentReservationDays = new int[numberOfReservations];
+            currentReservationRooms = new int[numberOfReservations];
+            for (int i = 0; i < numberOfReservations; i++) {
+                currentReservationDays[i] = Integer.parseInt(br.readLine());
+                currentReservationRooms[i] = Integer.parseInt(br.readLine());
+                if (currentReservationDays[i] == day && currentReservationRooms[i] == room) valid = true;
+            }
+            br.close();
+        } catch (IOException e) {
+            return false;
+        }
+        
+        if (!valid) {
+            System.out.println("This reservation does not exist");
+            return false;
+        }
+        
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter("reservations.txt", false));
+            bw.write(--numberOfReservations); br.newLine();
+            for (int i = 0; i < numberOfReservations; i++) {
+                if (!(room == currentReservationRooms[i] && day == currentReservationDays[i])) {
+                    bw.write(currentReservationDays[i]); bw.newLine();
+                    bw.write(currentReservationRooms[i]); bw.newLine();
+                }
+            }
+            bw.close();
+        } catch (IOException e) {
+            return false;
+        }
+        return true;   
     }
     public static int getInt() {
         Scanner sc = new Scanner(System.in);
